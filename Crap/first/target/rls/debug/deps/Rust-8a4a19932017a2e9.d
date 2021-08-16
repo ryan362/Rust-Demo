e5a2c02ee401a74cc5c8ddcef06611325a44fa03
ryan362 <59591086+ryan362@users.noreply.github.com>
2021-08-15 20:10:35 -0400

    Fix Random Folders

--- Crap/first/target/rls/debug/deps/Rust-8a4a19932017a2e9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\kanek\Documents\Rust\Crap\first\target\rls\debug\deps\Rust-8a4a19932017a2e9.rmeta: src\main.rs
-
-c:\Users\kanek\Documents\Rust\Crap\first\target\rls\debug\deps\Rust-8a4a19932017a2e9.d: src\main.rs
-
-src\main.rs:
